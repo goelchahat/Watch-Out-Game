7845c277e385ec975ba5e9ff87f987551c23c036
Chahat Goel <chahatgoel98@gmail.com>
2018-01-09 21:36:08 +0530

    initial commit

---
+++ android/src/com/chahatgoel/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chahatgoel.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chahatgoel.game.ZombieBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZombieBird(), config);
+	}
+}
